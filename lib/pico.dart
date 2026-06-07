d8a47b22cda1909ad70723e7592014c5a71fc875
Likhin M N <mnlikhin@gmail.com>
2026-06-07 17:15:40 +0530

    Feat: add DevTools middleware for Dart VM Service broadcasting

--- lib/pico.dart
+++ lib/pico.dart
@@ -2,6 +2,7 @@
 
 export 'src/async_value.dart';
 export 'src/async_value_extensions.dart';
+export 'src/pico_devtools.dart';
 export 'src/pico_builder.dart';
 export 'src/store.dart';
 export 'src/target_listener.dart';
---
+++ lib/src/pico_devtools.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+/// A middleware for [Store] that broadcasts state updates to the Dart VM Service.
+/// 
+/// This event stream can be intercepted by a custom Flutter DevTools extension 
+/// to visualize state history and enable time-travel debugging.
+/// 
+/// Usage: 
+/// ```dart
+/// final store = Store(
+///   initialState, 
+///   onUpdate: picoDevToolsMiddleware,
+/// );
+/// ```
+void picoDevToolsMiddleware<S>(S oldState, S newState) {
+  postEvent('pico:state_update', {
+    'oldState': oldState.toString(),
+    'newState': newState.toString(),
+    'timestamp': DateTime.now().toIso8601String(),
+  });
+}
