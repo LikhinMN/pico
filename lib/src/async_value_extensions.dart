37d44a80418ca109cbf6e9cceeed4aabe261075c
Likhin M N <mnlikhin@gmail.com>
2026-06-07 17:13:42 +0530

    Feat: implement AsyncValue quality of life extensions

---
+++ lib/src/async_value_extensions.dart
@@ -0,0 +1,28 @@
+import 'async_value.dart';
+
+extension AsyncValueX<T> on AsyncValue<T> {
+  /// Returns the underlying data if present, otherwise returns null.
+  T? get valueOrNull => when(
+        data: (data) => data,
+        error: (_, __) => null,
+        loading: () => null,
+      );
+
+  /// Returns the underlying data, throwing an error if it is not in the data state.
+  T get requireValue => when(
+        data: (data) => data,
+        error: (err, stack) {
+          Error.throwWithStackTrace(err, stack ?? StackTrace.current);
+        },
+        loading: () => throw StateError('Tried to call requireValue on an AsyncLoading state.'),
+      );
+
+  /// Maps the underlying data to a new type [R] if it is in the data state.
+  AsyncValue<R> map<R>(R Function(T data) mapper) {
+    return when(
+      data: (data) => AsyncData<R>(mapper(data)),
+      error: (err, stack) => AsyncError<R>(err, stack),
+      loading: () => AsyncLoading<R>(),
+    );
+  }
+}
