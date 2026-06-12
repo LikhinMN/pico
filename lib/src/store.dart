c096e017d08ab0dc51b9c9f5924589ff43b2c6cf
Likhin M N <mnlikhin@gmail.com>
2026-06-12 19:48:28 +0530

    feat(store): implement zustand-style subscriptions and harden batching loop
    Adds subscribe and subscribeWithSelector methods to Store for listening to state changes outside UI components. Hardens the microtask batching loop with try/finally to guarantee reset.

--- lib/src/store.dart
+++ lib/src/store.dart
@@ -31,12 +31,15 @@ class Store<S> {
     if (!_isBatching) {
       _isBatching = true;
       scheduleMicrotask(() {
-        for (final listener in _listeners) {
-          if (listener.updateAndCheck(_state)) {
-            listener.onRebuild();
+        try {
+          for (final listener in _listeners.toList()) {
+            if (listener.updateAndCheck(_state)) {
+              listener.onRebuild();
+            }
           }
+        } finally {
+          _isBatching = false;
         }
-        _isBatching = false;
       });
     }
   }
@@ -54,5 +57,76 @@ class Store<S> {
     } catch (e, stack) {
       set((state) => setter(state, AsyncError<T>(e, stack)));
     }
+  }
+
+  /// Subscribes to the store to listen for state changes.
+  ///
+  /// The [listener] will be called whenever the state changes.
+  /// If [fireImmediately] is true, the listener is called synchronously once upon subscription.
+  /// Returns a function that can be called to unsubscribe.
+  void Function() subscribe(
+    void Function(S state, S previousState) listener, {
+    bool fireImmediately = false,
+  }) {
+    S previousState = _state;
+
+    late final TargetListener<S, S> targetListener;
+
+    targetListener = TargetListener<S, S>(
+      selector: (s) => s,
+      initialState: _state,
+      onRebuild: () {
+        final currentState = targetListener.cachedSlice;
+        listener(currentState, previousState);
+        previousState = currentState;
+      },
+    );
+
+    addListener(targetListener);
+
+    if (fireImmediately) {
+      listener(previousState, previousState);
+    }
+
+    return () {
+      removeListener(targetListener);
+    };
+  }
+
+  /// Subscribes to a specific slice of the store's state.
+  ///
+  /// The [listener] will only be called when the value returned by [selector] changes.
+  /// If [fireImmediately] is true, the listener is called synchronously once upon subscription.
+  /// Returns a function that can be called to unsubscribe.
+  void Function() subscribeWithSelector<T>(
+    void Function(T value, T previousValue) listener, {
+    required T Function(S state) selector,
+    bool Function(T a, T b)? equals,
+    bool fireImmediately = false,
+  }) {
+    T previousSlice = selector(_state);
+
+    late final TargetListener<S, T> targetListener;
+
+    targetListener = TargetListener<S, T>(
+      selector: selector,
+      initialState: _state,
+      equals: equals,
+      onRebuild: () {
+        final currentSlice = targetListener.cachedSlice;
+        listener(currentSlice, previousSlice);
+        previousSlice = currentSlice;
+      },
+    );
+
+    addListener(targetListener);
+
+    if (fireImmediately) {
+      listener(previousSlice, previousSlice);
+    }
+
+    return () {
+      removeListener(targetListener);
+    };
   }
 }
